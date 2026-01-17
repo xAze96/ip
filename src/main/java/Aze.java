2ae8f8e2cacf306e8cf0b3d1a2d40572ddc53c3e
Eugene <eugenetanjy@gmail.com>
2026-01-17 14:25:25 +0800

    add support for list

--- src/main/java/Aze.java
+++ src/main/java/Aze.java
@@ -7,20 +7,33 @@ public class Aze {
         System.out.println("     What can I do for you?");
         System.out.println("    ____________________________________________________________\n");
         
+        String[] tasks = new String[100];
+        int taskNum = 0;
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
             }
-            System.out.println("    ____________________________________________________________");
-            System.out.println("     " + input);
-            System.out.println("    ____________________________________________________________\n");
+            switch (input) {
+                case "list":
+                    System.out.println("    ____________________________________________________________");
+                    for (int i = 0; i < taskNum; i++) {
+                        System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println("    ____________________________________________________________\n");
+                    break;
+                default:
+                    tasks[taskNum++] = input;
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     added: " + input);
+                    System.out.println("    ____________________________________________________________\n");
+            }
         }
         scanner.close();
 
         System.out.println("    ____________________________________________________________");
         System.out.println("     Bye. Hope to see you again soon!");
-        System.out.println("    ____________________________________________________________");
+        System.out.println("    ____________________________________________________________\n");
     }
 }
