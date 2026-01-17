95d71675f89a595fa509760728e9ae69a8c9dd59
Eugene <eugenetanjy@gmail.com>
2026-01-17 13:39:32 +0800

    Add greeting

---
+++ src/main/java/Aze.java
@@ -0,0 +1,13 @@
+public class Aze {
+    public static void main(String[] args) {
+        String greetString = "____________________________________________________________\n"
+                        + " Hello! I'm Aze\n"
+                        + " What can I do for you?\n"
+                        + "____________________________________________________________\n";
+        System.out.println(greetString);
+        
+        String exitString = " Bye. Hope to see you again soon!\n"
+                        + "____________________________________________________________";
+        System.out.println(exitString);
+    }
+}
